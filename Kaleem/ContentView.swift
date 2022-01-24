48f02ab62253942924a277624aa11c891ed80ce7
Noura-alhoshan <nooni-4321@hotmail.com>
2022-01-24 21:56:15 +0300

    First commit

---
+++ Kaleem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kaleem
+//
+//  Created by نوره سعد on 21/06/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kaleem/KaleemApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaleemApp.swift
+//  Kaleem
+//
+//  Created by نوره سعد on 21/06/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct KaleemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
