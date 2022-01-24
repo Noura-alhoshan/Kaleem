9e0d241b922b732bb0cc621d799242f44f3182b3
Noura-alhoshan <nooni-4321@hotmail.com>
2022-01-24 22:37:02 +0300

    test database

---
+++ Kaleem/testFirebase/model.swift
@@ -0,0 +1,15 @@
+//
+//  model.swift
+//  Kaleem
+//
+//  Created by نوره سعد on 21/06/1443 AH.
+//
+
+import Foundation
+
+struct Todo: Identifiable {
+    
+    var id: String
+    var name: String
+    var notes: String
+}
