9a7b69acf4462a3d05a53055edfc0759be6b443c
Raneem AlRashoud <439200695@student.ksu.edu.sa>
2022-02-02 06:46:06 +0300

    AppIcon

---
+++ Kaleem/View/SplashScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SplashScreen.swift
+//  Kaleem
+//
+//  Created by Raneem AlRashoud on 25/06/1443 AH.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+     Text("HELLO")
+    }
+}
+
+struct SplashScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreen()
+    }
+}
