51978cebd6e5544ea03f70267773cfbb97774713
Afnan Alzuayr <90301688+Afn4nz@users.noreply.github.com>
2022-02-25 03:50:29 +0300

    I added a button for video call function

--- Kaleem/View/ImpairedHome.swift
+++ Kaleem/View/ImpairedHome.swift
@@ -11,12 +11,33 @@ import Firebase
 
 struct ImpairedHome: View {
     @EnvironmentObject var session: SessionStore
+    @Environment(\.openURL) private var openURL
+    @State var showVideCall: Bool = false
     
    
     var body: some View {
         
         VStack{
             Text("Hello Impaired!")
+            
+            NavigationLink(destination: VideoCallV(), isActive: $showVideCall, label: {EmptyView()} )
+
+            /*Calling function*/
+            Button( action: {
+                self.showVideCall = true
+             /*   if let yourURL = URL(string: "facetime://0550804411") {
+                    UIApplication.shared.open(yourURL, options: [:], completionHandler: nil)
+                }*/
+
+            } , label: {
+                Text("اتصل")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 220, height: 60)
+                    .background(Color.black)
+                    .cornerRadius(35.0)
+            })
             
         Button(action: {
             session.signOut()
@@ -29,9 +50,11 @@ struct ImpairedHome: View {
                 .background(Color.black)
                 .cornerRadius(35.0)
         })
-        } //Text("looooong time").bold()
-    }
-}
+        } //Text("looooong time").bold() /VStack end
+        
+   
+    }// end body
+} //end view
 
 struct ImpairedHome_Previews: PreviewProvider {
     static var previews: some View {
