48f2d6b38593c757571f39b0269cc8233246145e
Sara Alshreef <59771807+SaraAbdullah7687@users.noreply.github.com>
2022-03-12 23:09:28 +0300

    Add files via upload

---
+++ Kaleem/Components/CustomAlertQuiz.swift
@@ -0,0 +1,101 @@
+//
+//  alert.swift
+//  Kaleem
+//
+//  Created by Afnan Al-Zuayr on 17/07/1443 AH.
+//
+
+import SwiftUI
+enum AlertAction2 {
+    
+    case ok
+    case cancel
+}
+struct CustomAlertQuiz: View {
+    @Binding var shown: Bool
+    @Binding var closureA: AlertAction2?
+    var oneBtn: Bool
+    @Binding var showNextPage : Bool
+       // var isSuccess: Bool
+    var imgName: String
+    var title: String
+    var message: String
+    var btn1: String
+    var btn2: String
+
+    @EnvironmentObject var quizManager1: AQuizManagerVM
+        var body: some View {
+            NavigationLink(destination: SignIn(), isActive: $showNextPage, label: {EmptyView()} )
+            ZStack{
+               
+            VStack {
+               
+                             
+                //Spacer()
+                Text(title).foregroundColor(Color.black).fontWeight(.bold).padding(.bottom, 5).padding(.top, 80)
+                Text(message).foregroundColor(Color.black)
+                Spacer()
+               // Divider()
+                HStack {
+                    //dialog btn 1
+                    Button(btn1) {
+                        closureA = .cancel
+                        shown.toggle()
+                        if  quizManager1.score > 5{
+                        quizManager1.updateStatus(status: "accepted")
+                        }else{
+                            //////////delete user
+                            quizManager1.deleteUser()
+                        }
+                        showNextPage = true
+                    }.frame(width: UIScreen.main.bounds.width/2-60, height: 40) // this btn width is consistent with the dialog box width
+                        .foregroundColor(.white)
+                        .background(Color("Kcolor"))
+                        .clipShape(Capsule())
+.padding(10)
+                    if (!oneBtn)
+                    {
+                    // dialog btn 2
+                    Button(btn2) {
+                        closureA = .ok
+                        shown.toggle()
+                    }.frame(width: UIScreen.main.bounds.width/2-60, height: 40) // this btn width is consistent with the dialog box width
+                        .foregroundColor(.white)
+                        .background(Color("Kcolor"))
+                        .clipShape(Capsule())
+                        .padding(10)
+                    }
+                }
+                
+            }
+            // dialog
+            .frame(width: UIScreen.main.bounds.width-50, height: 200)
+            .background(Color.white)
+            .cornerRadius(12)
+            .clipped()
+            
+            VStack{
+                Image(systemName: "checkmark.circle.fill").resizable().frame(width: 100, height: 100)//.cornerRadius(100)
+                    .background(Color.white.cornerRadius(50))
+               .overlay(Circle()
+                           .stroke(Color.white, lineWidth: 6)).foregroundColor(Color("Kcolor")).padding(.bottom, 180)
+       }
+            
+        }
+
+            .background(
+                Color.primary.opacity(0.35)
+                    .padding(-400)
+
+                    
+            )
+   
+        }
+}
+
+    /*
+struct alert_Previews: PreviewProvider {
+    static var previews: some View {
+        alert()
+    }
+}*/
