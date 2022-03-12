9f458eef4638c88ec629638c1000eb0ac06fbc43
Afnan Alzuayr <90301688+Afn4nz@users.noreply.github.com>
2022-03-12 17:19:22 +0300

    UPDATED signup VM

---
+++ Kaleem/ViewModel/SignUpVM2.swift
@@ -0,0 +1,196 @@
+import Foundation
+import SwiftUI
+import Firebase
+import Combine
+
+
+
+class SignUpVM : ObservableObject {
+    var email: String
+    init() {
+        self.email = ""
+    }
+    init(email: String) {
+        self.email = email
+    }
+
+    
+    let auth = Auth.auth()
+    let db = Firestore.firestore()
+    
+    
+    // create user in Firebase Auth.
+    func createNewAccount(email: String, password: String, userType: String, username: String, phone: String) { // I change it because it was display an error (var > String) ########
+        
+        auth.createUser(withEmail: email, password: password){ result, err in
+            if let err = err {
+                print("Failed to create user:", err)
+                return
+            }
+
+            print("Successfully created user: \(result?.user.uid ?? "")")
+
+            self.addDataToCollection(userType: userType, username: username, phone: phone, email: email)        }
+    } // END crrate user
+    
+     func addDataToCollection(userType: String, username: String, phone: String, email: String) {
+        
+         guard let uid = Auth.auth().currentUser?.uid else {
+             print ("Get FAILED!!!!!!!!!!!!!!!!!!!!!!111")
+             return }
+         print(uid)
+        // Add a document to a collection
+       //  db.collection(/*userType*/"Volunteer").addDocument(data: ["uid":uid/*, "username":username, "phoneNo":phone, "email":email*/])
+         // Add a document to a collection
+         db.collection(userType).document(uid).setData(["uid":uid,
+                                                        "username":username, "phoneNo":phone, "email":email, "userType": userType
+          ])         { error in
+            
+            // Check for errors
+            if error == nil {
+                // No errors
+            }
+            else {
+                // Handle the error
+            }
+        }
+    }
+   /*
+    // create user in Firebase Auth.
+    func createNewAccount(email: String, password: String) -> String{ // I change it because it was display an error (var > String) ########
+        var uid = ""
+        auth.createUser(withEmail: email, password: password){ result, err in
+            if let err = err {
+             //   print( err.localizedDescription)
+                return
+            }
+            uid = result?.user.uid ?? "NULL1234"
+            print("Successfully created user: \(result?.user.uid ?? "")")
+        }
+        return uid
+    } // END crrate user
+    
+    func addDataToCollection(uid: String, userType: String, username: String, phone: String, email: String) {
+        
+        
+
+        
+        // Add a document to a collection
+         db.collection(userType).document(uid).setData([
+            "username":username, "phoneNo":phone, "email":email
+         ]) { error in
+            
+            // Check for errors
+            if error == nil {
+                // No errors
+            }
+            else {
+                // Handle the error
+            }
+        }
+    }*/
+    func validateUserName (username: String) -> String {
+        var err = ""
+        if username == "" {
+            err = "*اسم المستخدم مطلوب"
+        }else if !username.containsOnlyLetters { // .containsOnlyLetters
+            err = "*يجب أن يكون الاسم أحرف فقط"
+        } else if username.count < 3 {
+            err = "*يجب أن يكون ٣ أحرف على الأقل"
+        }
+        return err
+    }
+    
+    func validatePhoneNo (phone: String)-> String {
+        var err = ""
+        if phone == "" {
+            err = "*رقم الجوال مطلوب"
+        } else if !phone.validPhone {
+            err = "* رقم الجوال يجب أن يبدأ بـ ٠٥ ويتكون من ١٠ خانات"
+        }
+      
+        return err
+    }
+    
+    func validateEmail (email: String) -> String {
+        var err = ""
+        if email == "" {
+            err = "*البريد الإلكتروني مطلوب"
+        }else if !email.isValidEmail {
+            err = "*البريد الإلكتروني غير صحيح"
+        }else {err = ""}
+ 
+        print(err) // =======
+        return err
+    }
+    
+    func isEmailExisted(handler: @escaping (Bool, String) -> Void) {
+        var isFound2 = false
+        if (self.email ).isValidEmail {
+            db.collection("Volunteer").getDocuments { (snapshot, error) in
+               if let error = error  {
+                   handler(false, error.localizedDescription)
+               } else {
+                   var isFound = false
+                   let allUser = snapshot?.documents
+                   for user in (allUser ?? []) {
+                       print ("EMAIL======================")
+                       let dbEmail = user["email"] as? String ?? ""
+                       if dbEmail == (self.email ) {
+                           isFound = true
+                           isFound2 = true
+                       }
+                   }
+                   if isFound {
+                       handler(false, "")
+                   }
+               } // end else
+                
+            }
+            if (isFound2 == false){
+            db.collection("Deaf").getDocuments { (snapshot, error) in
+               if let error = error  {
+                   handler(false, error.localizedDescription)
+               } else {
+                   var isFound = false
+                   let allUser = snapshot?.documents
+                   for user in (allUser ?? []) {
+                       let dbEmail = user["email"] as? String ?? ""
+                       if dbEmail == (self.email ) {
+                           isFound = true
+                       }
+                   }
+                   if isFound {
+                       handler(false, "")
+                   }
+               } // end else
+                
+            }
+        }
+        }
+    }
+    func validatePass (pass: String) -> String {
+        var err = ""
+        if pass == "" {
+            err = "*كلمة المرور مطلوبة"
+        }else if !pass.containsUppercase || !pass.containsNumbers || !pass.containsSpecialCharacters || pass.count < 7 {
+            err = "*كلمة المرور يجب أن تحتوي على حرف كبير، حرف صغير، رقم، رمز، و ٨ خانات على الأقل"
+        }
+        
+        return err
+    }
+    
+    func validateRepass (pass: String, repass: String)  -> String {
+        
+            var err = ""
+            if repass == "" {
+                err = "*تأكيد كلمة المرور مطلوب"
+            }  else if repass != pass {
+                err = "*لا يطابق كلمة المرور"
+            } else {err = "" }
+        
+            return err
+    }
+    
+}
+
