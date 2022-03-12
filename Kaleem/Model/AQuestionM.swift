d6f7d9d3314a6a59699903fb61edd05a6822805e
Noura Alhoshan <nooni-4321@hotmail.com>
2022-03-12 17:25:26 +0300

    Update AQuestionM.swift

--- Kaleem/Model/AQuestionM.swift
+++ Kaleem/Model/AQuestionM.swift
@@ -4,14 +4,16 @@
 //
 //  Created by نوره سعد on 26/06/1443 AH.
 //
-
 import Foundation
 
 struct QuestionModel: Identifiable {
     var id: UUID = UUID()
+    var Qid: String//added this one to update the document in Edit quiz
     var question: String
     var correctAnswer: String
+    var questionText: String
     var answers: [Answer]
+    
     
     struct Answer: Identifiable {
         var id: UUID = UUID()
